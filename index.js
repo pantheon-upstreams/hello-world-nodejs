c4f8b96ddea65591cfbe876c10586fcf95d1767c
coderkakarrot <pratik.coderx@gmail.com>
2025-03-19 23:34:09 +0530

    Removed hostname.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const { createServer } = require('node:http');
 
-const hostname = '127.0.0.1';
 const port = process.env.PORT || 3000;
 
 const server = createServer((req, res) => {
@@ -9,6 +8,6 @@ const server = createServer((req, res) => {
     res.end('Hello World');
 });
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, () => {
+    console.log(`Server running at http://0.0.0.0:${port}/`);
 });
